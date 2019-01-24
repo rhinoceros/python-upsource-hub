731e374fddbaee8cc8d065a4e053f30553172951
shijl <13735458727@139.com>
2019-01-24 13:57:17 +0800

    Update UpsourceClient.py

--- UpsourceClient.py
+++ UpsourceClient.py
@@ -32,8 +32,11 @@ class UpsourceClient:
         if response.status_code != 200:
             raise ConnectionError("Response: {} {}".format(response.status_code, response.text))
 
-    #获取所有项目id
     def get_all_project_ids(self):
+        """
+        获取所有项目id
+        :return:
+        """
         projects = self.GET('getAllProjects')
         if projects:
             project_ids = [project['projectId'] for project in projects['project']]
@@ -41,8 +44,11 @@ class UpsourceClient:
             project_ids = []
         return project_ids
 
-    #获取所有项目名称
     def get_all_project_names(self):
+        """
+        获取所有项目名称
+        :return:
+        """
         projects = self.GET('getAllProjects')
         if projects:
             project_names = [project['projectName'] for project in projects['project']]
@@ -50,49 +56,93 @@ class UpsourceClient:
             project_names = []
         return project_names
 
-    #获取项目属性(其中包含归属组信息)
     def get_project_attribute(self, project_id):
+        """
+        获取项目属性(其中包含归属组信息)
+        :param project_id:
+        :return:
+        """
         projects_attribute = self.GET('getProjectInfo', {'projectId': project_id})
         return projects_attribute
 
-    #获取项目设置
     def load_project_settings(self, project_id):
+        """
+        获取项目设置
+        :param project_id:
+        :return:
+        """
         settings = self.GET('loadProject', {'projectId': project_id})
         return settings
 
-    #重新设置项目属性
     def edit_project_settings(self, project_id, project_settings):
+        """
+        重新设置项目属性
+        :param project_id:
+        :param project_settings:
+        :return:
+        """
         self.POST('editProject', {'projectId': project_id, 'settings': project_settings})
 
-    #创建项目
     def create_project(self, project_id, project_settings):
+        """
+        创建项目
+        :param project_id:
+        :param project_settings:
+        :return:
+        """
         self.POST('createProject', {'newProjectId': project_id, 'settings': project_settings})
 
-    #删除项目
     def delete_project(self, project_id):
+        """
+        删除项目
+        :param project_id:
+        :return:
+        """
         self.POST('deleteProject', {'projectId': project_id})
 
-    #重置项目
     def reset_project(self, project_id):
-       self.POST('resetProject', {'projectId': project_id})
+        """
+        重置项目
+        :param project_id:
+        :return:
+        """
+        self.POST('resetProject', {'projectId': project_id})
 
-    #获取用户信息
     def load_user_info(self, user_id):
+        """
+        获取用户信息
+        :param user_id:
+        :return:
+        """
         infos = self.GET('getUserInfo', {'ids': user_id})['infos'][0]
         if 'login' in infos:
             return infos
         else:
             return None
 
-    #将用户添加到项目中
     def add_user_to_project(self, project_id, user_id):
+        """
+        将用户添加到项目中
+        :param project_id:
+        :param user_id:
+        :return:
+        """
         self.POST('addUserRole', {'projectId': project_id, 'userId': user_id, 'roleKey': 'developer'})
 
-    #将用户从项目中移除
     def delete_user_from_project(self, project_id, user_id):
+        """
+        将用户从项目中移除
+        :param project_id:
+        :param user_id:
+        :return:
+        """
         self.POST('deleteUserRole', {'projectId': project_id, 'userId': user_id, 'roleKey': 'developer'})
 
-    #获取用户在项目中的权限
     def load_user_roles_in_project(self, project_id):
+        """
+        获取用户在项目中的权限
+        :param project_id:
+        :return:
+        """
         user_roles = self.GET('getUsersRoles', {'projectId': project_id, 'offset': 0, 'pageSize': 1000})
         return user_roles
